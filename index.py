537be56c483eefdd006e56c80cd25aaac30f61c9
GianmarcoIC <i2415363@continental.edu.pe>
2024-12-10 21:23:14 -0500

    Update index.py

--- index.py
+++ index.py
@@ -6,7 +6,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 from sklearn.preprocessing import StandardScaler
-
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
